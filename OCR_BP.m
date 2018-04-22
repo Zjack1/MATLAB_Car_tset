bec0cbba9fd8731ac722689fddc473af02bcb719
Jack <37362585+Zjack1@users.noreply.github.com>
2018-04-22 14:11:16 +0800

    Create OCR_BP.m

---
+++ OCR_BP.m
@@ -0,0 +1,19 @@
+clc;
+clear all;
+close all;
+J=imread('D:\桌面\测试车牌\总字符1\3.jpg');
+% figure,imshow(J);
+% 
+% I=imread('D:\桌面\测试车牌\标准字符1\黑.bmp');
+% figure,imshow(I);
+% 
+% % imwrite(J,'D:\桌面\测试车牌\分割字符\9.bmp');
+% B=imread('D:\桌面\测试车牌\标准字符1\辽.bmp');
+% figure,imshow(B);
+% 
+% Z=imsubtract(J,I);
+% figure,imshow(Z);
+% Z1=imsubtract(J,B);
+% figure,imshow(Z1);
+% d=sum(sum(Z))
+% d1=sum(sum(Z1))
